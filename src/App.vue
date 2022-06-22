e825ce3b7d1bd912636ea5f5e21c6723911779f9
Breno Alvim <alvimbrenosilva6@gmail.com>
2022-06-22 19:50:34 -0300

    background component login

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,39 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <link
+      rel="shortcut icon"
+      href="../src/assets/facilita.png"
+      type="image/x-icon"
+    />
+    <Login />
   </div>
 </template>
 
 <script>
+import Login from "./components/Login";
+
 export default {
-  name: 'App'
-}
+  name: "App",
+  components: {
+    Login
+  }
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+:root {
+  --brand-color: #2693ff;
+  --light-color: #ffffff;
+  --font-family: "gilroy", arial;
+}
+* {
+  font-family: "gilroy", arial;
+  margin: 0;
+  padding: 0;
+}
+html {
+  display: flex;
+  justify-content: center;
+  align-content: center;
 }
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="login">
+    <div class="info">
+      <!-- <div class="image"> -->
+        <!-- <img src="../assets/facilita.png" alt="" /> -->
+      <!-- </div> -->
+      <span class="title"> <strong>FACILITA</strong>TASK </span>
+      <span class="subtitle">
+        Gestão de tarefas pessoais
+      </span>
+    </div>
+    <div class="form"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.login {
+  width: 100vw;
+  height: 100vh;
+  background: var(--light-color);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
+.form,
+.info {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  height: 100vh;
+}
+
+.info {
+  width: 40%;
+  background: var(--brand-color);
+}
+
+.form {
+  width: 59.89%;
+}
+
+.info .image {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  width: 100%;
+  height: 100%;
+}
+
+.info img {
+  width: 100px;
+  height: 100px;
+  border-radius: 8px;
+}
+
+.info span {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+}
+</style>
